5a11951a938bbf492d29dcc3e13e123236c18bff
otman lahreche <otman.lahreche@gmail.com>
2023-10-30 10:34:41 +0100

    Add project files.

---
+++ Test/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            label3 = new Label();
+            label4 = new Label();
+            listBox1 = new ListBox();
+            button2 = new Button();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            label6 = new Label();
+            button3 = new Button();
+            button4 = new Button();
+            label7 = new Label();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            checkBox3 = new CheckBox();
+            checkBox4 = new CheckBox();
+            listBox2 = new ListBox();
+            label8 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.ImeMode = ImeMode.NoControl;
+            textBox1.Location = new Point(12, 34);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(253, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(44, 15);
+            label1.TabIndex = 1;
+            label1.Text = "num01";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 72);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 15);
+            label2.TabIndex = 3;
+            label2.Text = "num02";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(12, 90);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(253, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(175, 135);
+            button1.Name = "button1";
+            button1.Size = new Size(90, 23);
+            button1.TabIndex = 4;
+            button1.Text = "empty inputs";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            comboBox1.AutoCompleteSource = AutoCompleteSource.ListItems;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "item01", "item02", "item03", "item04", "item05", "item06" });
+            comboBox1.Location = new Point(12, 205);
+            comboBox1.Margin = new Padding(2);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(253, 23);
+            comboBox1.TabIndex = 5;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 238);
+            label3.Margin = new Padding(2, 0, 2, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(81, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Selected item:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(102, 238);
+            label4.Margin = new Padding(2, 0, 2, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 7;
+            label4.Text = "label4";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Items.AddRange(new object[] { "listbox_item01", "listbox_item02", "listbox_item03", "listbox_item04", "listbox_item05", "listbox_item06", "listbox_item07", "listbox_item08", "listbox_item09", "listbox_item10" });
+            listBox1.Location = new Point(12, 276);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(253, 109);
+            listBox1.TabIndex = 8;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(94, 135);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 9;
+            button2.Text = "add to list";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 430);
+            label5.Name = "label5";
+            label5.Size = new Size(42, 15);
+            label5.TabIndex = 10;
+            label5.Text = "Name:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 448);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(253, 23);
+            textBox3.TabIndex = 11;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(37, 493);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(51, 19);
+            radioButton1.TabIndex = 12;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Male";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(94, 493);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(63, 19);
+            radioButton2.TabIndex = 13;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Female";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 532);
+            label6.Name = "label6";
+            label6.Size = new Size(25, 15);
+            label6.TabIndex = 14;
+            label6.Text = "......";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(190, 558);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 15;
+            button3.Text = "show";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(109, 558);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 16;
+            button4.Text = "reset";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(316, 16);
+            label7.Name = "label7";
+            label7.Size = new Size(59, 15);
+            label7.TabIndex = 17;
+            label7.Text = "Modules :";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(326, 38);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(83, 19);
+            checkBox1.TabIndex = 18;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(326, 63);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(83, 19);
+            checkBox2.TabIndex = 19;
+            checkBox2.Text = "checkBox2";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckedChanged += checkBox2_CheckedChanged;
+            // 
+            // checkBox3
+            // 
+            checkBox3.AutoSize = true;
+            checkBox3.Location = new Point(326, 88);
+            checkBox3.Name = "checkBox3";
+            checkBox3.Size = new Size(83, 19);
+            checkBox3.TabIndex = 20;
+            checkBox3.Text = "checkBox3";
+            checkBox3.UseVisualStyleBackColor = true;
+            checkBox3.CheckedChanged += checkBox3_CheckedChanged;
+            // 
+            // checkBox4
+            // 
+            checkBox4.AutoSize = true;
+            checkBox4.Location = new Point(326, 113);
+            checkBox4.Name = "checkBox4";
+            checkBox4.Size = new Size(83, 19);
+            checkBox4.TabIndex = 21;
+            checkBox4.Text = "checkBox4";
+            checkBox4.UseVisualStyleBackColor = true;
+            checkBox4.CheckedChanged += checkBox4_CheckedChanged;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(446, 38);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(120, 94);
+            listBox2.TabIndex = 22;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(427, 16);
+            label8.Name = "label8";
+            label8.Size = new Size(106, 15);
+            label8.TabIndex = 23;
+            label8.Text = "Selected Modules :";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1189, 686);
+            Controls.Add(label8);
+            Controls.Add(listBox2);
+            Controls.Add(checkBox4);
+            Controls.Add(checkBox3);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(label7);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(label6);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(textBox3);
+            Controls.Add(label5);
+            Controls.Add(button2);
+            Controls.Add(listBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Margin = new Padding(2);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button1;
+        private ComboBox comboBox1;
+        private Label label3;
+        private Label label4;
+        private ListBox listBox1;
+        private Button button2;
+        private Label label5;
+        private TextBox textBox3;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Label label6;
+        private Button button3;
+        private Button button4;
+        private Label label7;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private ListBox listBox2;
+        private Label label8;
+    }
+}
---
+++ Test/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string t1 = textBox1.Text;
+            string t2 = textBox2.Text;
+
+            if (t1 == "" || t2 == "")
+            {
+                MessageBox.Show("the input boxs are empty already!!");
+            }
+            else
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label4.Text = comboBox1.SelectedItem.ToString();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string s1 = textBox1.Text;
+            if (s1 == "")
+            {
+                MessageBox.Show("text field_01 is empty");
+
+            }
+            else
+            {
+                listBox1.Items.Add(s1);
+                textBox1.Text = "";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "")
+            {
+                MessageBox.Show("input name is empty!");
+            }
+            else
+            {
+                if (radioButton1.Checked)
+                {
+                    label6.Text = "Your name is : " + textBox3.Text + "  and your gender is : " + radioButton1.Text;
+                }
+                else if (radioButton2.Checked)
+                {
+                    label6.Text = "Your name is : " + textBox3.Text + "  and your gender is : " + radioButton2.Text;
+                }
+                else
+                {
+                    MessageBox.Show("select a gender!");
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label6.Text = "......";
+            textBox3.Text = "";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                listBox2.Items.Add(checkBox1.Text);
+            }
+            else
+            {
+                listBox2.Items.Remove(checkBox1.Text);
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                listBox2.Items.Add(checkBox2.Text);
+            }
+            else
+            {
+                listBox2.Items.Remove(checkBox2.Text);
+            }
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                listBox2.Items.Add(checkBox3.Text);
+            }
+            else
+            {
+                listBox2.Items.Remove(checkBox3.Text);
+            }
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox4.Checked)
+            {
+                listBox2.Items.Add(checkBox4.Text);
+            }
+            else
+            {
+                listBox2.Items.Remove(checkBox4.Text);
+            }
+        }
+    }
+}
